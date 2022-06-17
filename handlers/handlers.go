00e3e1bdaf2ca2ca7e1d70b08c20120fa2001c56
cap7_nemo <adhyttungga.jkt@gmail.com>
2022-06-17 08:33:54 +0700

    add middleware

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 
@@ -8,17 +9,30 @@ import (
 	"github.com/rs/cors"
 )
 
+func MiddlewareAuth(next http.Handler) http.Handler {
+	return http.HandlerFunc(func (w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(make(map[string]interface{}))
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 func HandleReq() {
 	log.Println("Start development server localhost:9999")
 
 	myRouter := mux.NewRouter().StrictSlash(true)
 
 	myRouter.HandleFunc("/", HomePage)
-	myRouter.HandleFunc("/article", CreateArticle).Methods("OPTIONS", "POST")
-	myRouter.HandleFunc("/article/{limit}/{offset}", GetArticles).Methods("OPTIONS", "GET")
-	myRouter.HandleFunc("/article/{id}", GetArticle).Methods("OPTIONS", "GET")
-	myRouter.HandleFunc("/article/{id}", UpdateArticle).Methods("OPTIONS", "PUT")
-	myRouter.HandleFunc("/article/{id}", DeleteArticle).Methods("OPTIONS", "Delete")
+	myRouter.Handle("/article", MiddlewareAuth(http.HandlerFunc(CreateArticle))).Methods("OPTIONS", "POST")
+	myRouter.Handle("/article/{limit}/{offset}", MiddlewareAuth(http.HandlerFunc(GetArticles))).Methods("OPTIONS", "GET")
+	myRouter.Handle("/article/{id}", MiddlewareAuth(http.HandlerFunc(GetArticle))).Methods("OPTIONS", "GET")
+	myRouter.Handle("/article/{id}", MiddlewareAuth(http.HandlerFunc(UpdateArticle))).Methods("OPTIONS", "PUT")
+	myRouter.Handle("/article/{id}", MiddlewareAuth(http.HandlerFunc(DeleteArticle))).Methods("OPTIONS", "Delete")
 
 	handler := cors.AllowAll().Handler(myRouter)
 	
